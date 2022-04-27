00872d5f8bee1f8597dfae5976d16c0cd976d440
gabrielricardosilva <gabrielricardo.silva17@gmail.com>
2022-04-27 09:29:25 -0300

    first commit

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+function somar(){
+    let n1 = document.getElementById('n1').value
+    let n2 = document.getElementById('n2').value
+
+    document.getElementById('Resultado').innetHTML = n1 + n2;
+}
